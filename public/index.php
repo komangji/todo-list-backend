335fbd638554df857d6feed39b76e5864f600533
sametamahesuari-gif <sametamahesuari@gmail.com>
2025-12-28 21:03:11 +0800

    membuat tampilan index.php dan koneksi database

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+// Memanggil file koneksi database
+include "../config/database.php";
+?>
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Todo List</title>
+</head>
+<body>
+    <h1>Todo List</h1>
+    <p>Halaman utama aplikasi Todo List</p>
+</body>
+</html>
