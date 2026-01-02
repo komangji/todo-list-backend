9c1e156482a31caa4e9c08ce586fa06ff142a270
komangji <komangarinugraha14@gmail.com>
2026-01-02 13:10:29 +0800

    memperbaiki penamaan class model Todo serta memperbaiki bug pada query update

--- models/todoModel.php
+++ models/todoModel.php
@@ -1,41 +1,53 @@
 <?php
-class todoModel {
+class Todo {
     private $conn;
-    public function __construct($db){
+
+    // Konstruktor
+    public function __construct($db) {
         $this->conn = $db;
     }
 
+    // Ambil semua data todo
     public function getAll() {
         $query = "SELECT * FROM todos ORDER BY created_at DESC";
         return mysqli_query($this->conn, $query);
     }
 
+    // Ambil data berdasarkan ID
     public function getById($id) {
         $query = "SELECT * FROM todos WHERE id = $id";
         return mysqli_query($this->conn, $query);
     }
 
-    public function insert($data){
+    // Tambah data
+    public function insert($data) {
         $title = $data['title'];
         $description = $data['description'];
 
-        $query = "INSERT INTO todos (title, description) VALUES ('$title', '$description')";
+        $query = "INSERT INTO todos (title, description) 
+                  VALUES ('$title', '$description')";
 
         return mysqli_query($this->conn, $query);
     }
 
+    // Update data
     public function update($id, $data) {
         $title = $data['title'];
         $description = $data['description'];
         $status = $data['status'];
 
-        $query = "UPDATE todos SET title = '$title', desription = '$description', status = '$status' WHERE id = $id";
+        $query = "UPDATE todos 
+                  SET title = '$title', 
+                      description = '$description', 
+                      status = '$status' 
+                  WHERE id = $id";
 
         return mysqli_query($this->conn, $query);
     }
-    
+
+    // Hapus data
     public function delete($id) {
         $query = "DELETE FROM todos WHERE id = $id";
         return mysqli_query($this->conn, $query);
     }
-}   
+}
