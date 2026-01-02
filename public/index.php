3768b3a5fd5adb47e7800403b1c4d23505ba13f9
sametamahesuari-gif <sametamahesuari@gmail.com>
2026-01-02 12:54:21 +0800

    menambahkan tampilan read

--- public/index.php
+++ public/index.php
@@ -1,6 +1,9 @@
 <?php
-// Memanggil file koneksi database
-include "../config/database.php";
+require_once '../config/database.php';
+require_once '../controllers/todoController.php';
+
+$controller = new TodoController($conn);
+$todos = $controller->index();
 ?>
 
 <!DOCTYPE html>
@@ -8,9 +11,37 @@ include "../config/database.php";
 <head>
     <meta charset="UTF-8">
     <title>Todo List</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 <body>
-    <h1>Todo List</h1>
-    <p>Halaman utama aplikasi Todo List</p>
+
+<h2>Daftar Todo</h2>
+
+<a href="create.php">+ Tambah Todo</a>
+
+<table>
+    <tr>
+        <th>No</th>
+        <th>Judul</th>
+        <th>Deskripsi</th>
+        <th>Status</th>
+        <th>Aksi</th>
+    </tr>
+
+    <?php $no = 1; while ($row = mysqli_fetch_assoc($todos)) : ?>
+    <tr>
+        <td><?= $no++ ?></td>
+        <td><?= $row['title'] ?></td>
+        <td><?= $row['description'] ?></td>
+        <td><?= $row['status'] ?></td>
+        <td>
+            <a href="edit.php?id=<?= $row['id'] ?>">Edit</a> |
+            <a href="delete.php?id=<?= $row['id'] ?>" 
+               onclick="return confirm('Yakin hapus data?')">Hapus</a>
+        </td>
+    </tr>
+    <?php endwhile; ?>
+</table>
+
 </body>
 </html>
