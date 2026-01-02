407fd32ca597722086e75236a1abd5c527af183d
komangji <komangarinugraha14@gmail.com>
2026-01-02 14:45:28 +0800

    memperbaiki codingan todoController.php

--- controllers/todoController.php
+++ controllers/todoController.php
@@ -1,37 +1,40 @@
 <?php
+// Memanggil file model Todo
 require_once '../models/todoModel.php';
 
+// Controller bertugas sebagai penghubung antara View dan Model
 class TodoController {
+
+    // Properti untuk menyimpan objek model
     private $model;
 
-    //membuat object model
-    public function __construct($db){
+    // Constructor untuk membuat objek model dengan koneksi database
+    public function __construct($db) {
         $this->model = new Todo($db);
     }
 
-    //menampilkan semua data todo
-    public function index(){
+    // Method untuk menampilkan seluruh data todo
+    public function index() {
         return $this->model->getAll();
     }
 
-    //menyimpan data todo baru
-    public function store($data){
+    // Method untuk menyimpan data todo baru
+    public function store($data) {
         return $this->model->insert($data);
     }
 
-    //mengambil satu data todo berdasarkan ID
-    public function show($id){
+    // Method untuk mengambil satu data todo berdasarkan ID
+    public function show($id) {
         return $this->model->getById($id);
     }
 
-    //memperbarui data todo
-    public function update($id, $data){
+    // Method untuk memperbarui data todo
+    public function update($id, $data) {
         return $this->model->update($id, $data);
     }
 
-    //menghapus data todo
-    public function destroy($id)
-    {
+    // Method untuk menghapus data todo
+    public function destroy($id) {
         return $this->model->delete($id);
     }
 }
